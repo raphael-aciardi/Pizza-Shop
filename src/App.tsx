75c1e5ddc085ff4cc2203656a746e63e81f22025
Raphael Aciardi <109046764+raphael-aciardi@users.noreply.github.com>
2024-12-18 15:50:04 -0300

    Creating routes and layouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import './index.css'
-import { Button } from './components/ui/button'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes'
 
 export function App() {
-  
   return (
-    <div className='flex justify-center items-center h-screen'>
-      <Button>Teste</Button>
-    </div>
+    <RouterProvider router={router} />
   )
 }
 
---
+++ src/pages/auth/sign-in.tsx
@@ -0,0 +1,5 @@
+export function SingIn() {
+    return (
+        <h1>SignIn</h1>
+    )
+}
