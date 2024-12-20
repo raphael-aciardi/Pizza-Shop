c6fafda1a0569be6713457391201635c527c180c
Raphael Aciardi <109046764+raphael-aciardi@users.noreply.github.com>
2024-12-20 10:32:06 -0300

    Creating Header

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -38,7 +38,9 @@ export function SingIn() {
             <Helmet title="Login" />
             <div className="p-8">
                 <Button variant='secondary' asChild className="absolute top-8 right-8">
-                    <Link to="/sign-up" className="text-sm text-muted-foreground">Novo estabelecimento</Link>
+                    <Link to="/sign-up">
+                        Novo estabelecimento
+                    </Link>
                 </Button>
                 <div className="w-[350px] flex flex-col justify-center gap-6">
                     <div className="flex flex-col gap-2 text-center">
--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -5,7 +5,7 @@ import { Helmet } from "react-helmet-async"
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { toast } from "sonner"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 const signUpForm = z.object({
     restaurantName: z.string(),
@@ -19,20 +19,25 @@ type SignUpForm = z.infer<typeof signUpForm>
 export function SignUp() {
     const { register, handleSubmit, formState: { isSubmitting } } = useForm<SignUpForm>()
 
+    const navigate = useNavigate()
+
+
     async function handleSignUp(data: SignUpForm) {
 
         try {
 
             await new Promise(resolve => setTimeout(resolve, 2000))
 
-            // toast.success('Enviamos um link de autenticação para seu e-mail.', {
-            //     action: {
-            //        label: 'Reenviar',
-            //        onClick: () => handleSignUp(data)
-            //     }
-            // })
+            console.log(data)
+
+            toast.success('Resrtaurante cadastrado com sucesso', {
+                action: {
+                    label: 'Login',
+                    onClick: () => navigate('/sign-in')
+                }
+            })
         } catch {
-            toast.error("credenciais inválidas")
+            toast.error("Erro ao cadastrar restaurante")
         }
     }
 
@@ -41,7 +46,9 @@ export function SignUp() {
             <Helmet title="Cadastro" />
             <div className="p-8">
                 <Button variant='secondary' asChild className="absolute top-8 right-8">
-                    <Link to="/sign-in" className="text-sm text-muted-foreground">Fazer login</Link>
+                    <Link to="/sign-in">
+                        Fazer login
+                    </Link>
                 </Button>
                 <div className="w-[350px] flex flex-col justify-center gap-6">
                     <div className="flex flex-col gap-2 text-center">
@@ -51,11 +58,27 @@ export function SignUp() {
 
                     <form action="" className="space-y-4" onSubmit={handleSubmit(handleSignUp)}>
                         <div className="space-y-2">
+                            <Label htmlFor="restaurantName">Nome do estabelecimento</Label>
+                            <Input id="restaurantName" type="text" {...register("restaurantName")} />
+                        </div>
+
+                        <div className="space-y-2">
+                            <Label htmlFor="managerName">Seu nome</Label>
+                            <Input id="managerName" type="text" {...register("managerName")} />
+                        </div>
+
+                        <div className="space-y-2">
                             <Label htmlFor="email">Seu e-mail</Label>
                             <Input id="email" type="email" {...register("email")} />
                         </div>
 
+                        <div className="space-y-2">
+                            <Label htmlFor="phone">Seu número de celular</Label>
+                            <Input id="phone" type="tel" {...register("phone")} />
+                        </div>
+
                         <Button disabled={isSubmitting} className="w-full" type="submit">Finalizar cadastro</Button>
+                        <p className="px-5 text-center text-sm loading-relaxed text-muted-foreground">Ao continuar, você concorda com nossos<a className="underline underline-offset-4" href="#"> termos de serviço</a> e{' '} <a className="underline underline-offset-4" href="">políticas de privacidade.</a> </p>
                     </form>
                 </div>
             </div>
