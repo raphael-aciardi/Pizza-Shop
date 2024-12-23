20c2e97237ed11e1badba41330e8cd18947acd3d
Raphael Aciardi <109046764+raphael-aciardi@users.noreply.github.com>
2024-12-23 11:19:19 -0300

    Creating menu options

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { Toaster } from 'sonner'
+import { ThemeProvider } from './components/theme/theme-provider'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate='%s | Pizza Shop' />
-      <Toaster richColors  />
-      <RouterProvider router={router} />
-    </HelmetProvider>
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+        <Helmet titleTemplate='%s | Pizza Shop' />
+        <Toaster richColors />
+        <RouterProvider router={router} />
+      </ThemeProvider >
+    </HelmetProvider >
   )
 }
 
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,15 @@
 import { Home, Pizza, UtensilsCrossed } from "lucide-react";
 import { Separator } from "./ui/separator";
 import { NavLink } from "./nav-link";
+import { ThemeToggle } from "./theme/theme-toggle";
+import { AccountMenu } from "./acount-menu";
 
 export function Header() {
     return (
         <div className="border-b">
-            <div className="flex h-16 itams-center gap-6 px-6">
+            <div className="flex h-16 items-center gap-6 px-6">
                 <Pizza className="h-6 w-6" />
                 <Separator orientation="vertical" className="h-6" />
-
                 <nav className="flex items-center space-x-4 lg:space-x-6">
                     <NavLink to={'/'}>
                         <Home className="h-4 w-4" />
@@ -19,6 +20,10 @@ export function Header() {
                         Pedidos
                     </NavLink>
                 </nav>
+                <div className="ml-auto flex items-center gap-2">
+                    <ThemeToggle />
+                    <AccountMenu />
+                </div>
             </div>
         </div>
     )
